bcaaf26880147be6b94692b972c7ba46a66bfaf6
Ramazan Sakin <ramazansakin63@gmail.com>
2020-02-16 16:21:07 +0300

    Sample Component Def impl

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import SampleComponent from './components/SampleComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <TestComponent></TestComponent>
     </div>
   );
 }
+
+class TestComponent extends Component{
+  render(){
+    return (
+      <div className="testComponent">
+      Test Component
+      <SampleComponent/>
+    </div>
+    );
+  }
+}
+
 
 export default App;
---
+++ src/components/SampleComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class TestComponent2 extends Component{
+    render(){
+      return (
+        <div className="testComponent2">
+        Test Component 2
+      </div>
+      );
+    }
+  }
